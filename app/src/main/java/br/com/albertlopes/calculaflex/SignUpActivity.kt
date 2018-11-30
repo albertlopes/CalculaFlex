cf10c0a01236670b2702eaba79795af043c690db
albertlopes <gh444522487>
2018-11-29 23:04:20 -0200

    Salvando

--- app/src/main/java/br/com/albertlopes/calculaflex/SignUpActivity.kt
+++ app/src/main/java/br/com/albertlopes/calculaflex/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import br.com.albertlopes.calculaflex.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_sign_up.*
